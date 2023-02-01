aab985a441dbd4d0549353f1e2354ac3862c65db
Antonio Macaringue <110739274+Makas-M@users.noreply.github.com>
2023-02-01 19:03:38 +0200

    init

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
-
-
 <?php
+//receber as datas
 $data1 = $_GET['data1'];
 $data2 = $_GET['data2'];
-
+//transformar as datas em segundos
 $d1 = strtotime("$data1");
 $d2 = strtotime("$data2");
-
+//fazer a subtracao entre as datas
 $dataFinal = ($d2-$d1)/86400;
-
+//condicao para nao haverem dados negativos
 if ($d2 < $d1) {
-	$dataFinal = $dataFinal * (-1);
-
-	
+	$dataFinal = $dataFinal * (-1);	
 }
+//saida
 echo $dataFinal;
 
 ?>
