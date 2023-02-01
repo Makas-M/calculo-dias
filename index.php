877991d1c55c524557aa675aed19243d7e037b94
Antonio Macaringue <110739274+Makas-M@users.noreply.github.com>
2023-02-01 18:57:46 +0200

    init

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
+
+
 <?php
 $data1 = $_GET['data1'];
 $data2 = $_GET['data2'];
-
-//$data1 = explode('/', $data1);
-//$data2 = explode('/', $data2);
 
 $d1 = strtotime("$data1");
 $d2 = strtotime("$data2");
 
 $dataFinal = ($d2-$d1)/86400;
+
+if ($d2 < $d1) {
+	$dataFinal = $dataFinal * (-1);
+
+	
+}
 echo $dataFinal;
+
 ?>
